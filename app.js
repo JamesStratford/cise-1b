7114540c04429d74e7f61ce28b002009f4c1a9c5
JamesStratford <james.e.stratford@gmail.com>
2022-09-19 22:44:39 +1200

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// app.js
+
+const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const connectDB = require('./config/db');
+var cors = require('cors');
+
+// routes
+const books = require('./routes/api/books.js');
+
+const app = express();
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+// Connect Database
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// use Routes
+app.use('/api/books', books);
+
+// Accessing the path module ---- Heroku deployment
+// Step 1:
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static("my-app/build"));
+
+    app.get("*", (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, "my-app", "build", "index.html"));
+
+   });
+
+}
+// Step 2:
+// ------------------------------------------------
+
---
+++ my-app/src/components/BookCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const BookCard = (props) => {
+    const  book  = props.book;
+
+    return(
+        <div className="card-container">
+            <img src="https://smartmobilestudio.com/wp-content/uploads/2012/06/leather-book-preview-300x252.png" alt="" />
+            <div className="desc">
+                <h2>
+                    <Link to={`/show-book/${book._id}`}>
+                        { book.title }
+                    </Link>
+                </h2>
+                <h3>{book.author}</h3>
+                <p>{book.description}</p>
+            </div>
+        </div>
+    )
+};
+
+export default BookCard;
