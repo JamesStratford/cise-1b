5b8960bcd68a24d5f2e100f20bfe59b2a6705e25
JamesStratford <james.e.stratford@gmail.com>
2022-09-26 15:23:39 +1300

    Created table_data branch for creating the data table

---
+++ my-app/src/components/TableData.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { useTable } from 'react-table';
+
+class TableData extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data:[]
+      };
+    }
+
+    
+
+}
