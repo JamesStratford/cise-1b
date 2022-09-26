e29381fa65f432bcdcc8a7d547fed8a24fb9e585
JamesStratford <james.e.stratford@gmail.com>
2022-09-26 15:56:55 +1300

    Created AddArticle.js

---
+++ my-app/src/components/AddArticle.js
@@ -0,0 +1,179 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import axios from 'axios';
+
+
+class AddArticle extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      author:'',
+      journal_name:'',
+      published_date:'',
+      volume:'',
+      number:'',
+      pages:'',
+      doi:''
+    };
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const data = {
+      title: this.state.title,
+      author: this.state.author,
+      journal_name: this.state.journal_name,
+      published_date: this.state.published_date,
+      volume: this.state.volume,
+      number: this.state.number,
+      pages: this.state.pages,
+      doi: this.state.doi
+    };
+
+    axios
+      .post('/api/articles', data)
+      .then(res => {
+        this.setState({
+            title: '',
+            author:'',
+            journal_name:'',
+            published_date:'',
+            volume:'',
+            number:'',
+            pages:'',
+            doi:''
+        })
+        this.props.history.push('/');
+      })
+      .catch(err => {
+        console.log("Error in CreateArticle!");
+      })
+  };
+
+  render() {
+    return (
+      <div className="CreateArticle">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <br />
+              <Link to="/" className="btn btn-outline-warning float-left">
+                  Show BooK List
+              </Link>
+            </div>
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Add Book</h1>
+              <p className="lead text-center">
+                  Create new book
+              </p>
+
+              <form noValidate onSubmit={this.onSubmit}>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Title of the Article'
+                    name='title'
+                    className='form-control'
+                    value={this.state.title}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <br />
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Author'
+                    name='author'
+                    className='form-control'
+                    value={this.state.author}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Journal Name'
+                    name='description'
+                    className='form-control'
+                    value={this.state.journal_name}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='date'
+                    placeholder='Published date'
+                    name='published_date'
+                    className='form-control'
+                    value={this.state.published_date}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Volume'
+                    name='volume'
+                    className='form-control'
+                    value={this.state.volume}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Number'
+                    name='number'
+                    className='form-control'
+                    value={this.state.number}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Pages'
+                    name='pages'
+                    className='form-control'
+                    value={this.state.pages}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='DOI number'
+                    name='doi'
+                    className='form-control'
+                    value={this.state.doi}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <input
+                    type="submit"
+                    className="btn btn-outline-warning btn-block mt-4"
+                />
+              </form>
+          </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreateArticle;
