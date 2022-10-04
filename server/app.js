ab26b4551abf1add4285aca63b0d0fafb0e51ca5
JamesStratford <james.e.stratford@gmail.com>
2022-10-04 16:09:00 +1300

    Fixing API

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
 
-    app.use(express.static("frontend/build"));
+    app.use(express.static("../frontend/build"));
 
     app.get("*", (req, res) => {
 
