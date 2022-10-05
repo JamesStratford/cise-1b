fdeceb0577aab2f3d81dee7cbebdc1e9805bda32
JamesStratford <james.e.stratford@gmail.com>
2022-10-05 13:13:20 +1300

    removed bad test

--- server/app.test.js
+++ server/app.test.js
@@ -1,18 +1,4 @@
-const connectDB = require('./config/db');
-
-const articles = require('./routes/api/articles.js');
-
 test('base test', () => {
-    pass = 1;
-    connectDB();
-    articles.get('/api/articles')
-    .then(res => {
-    this.setState({
-        articles: res.data
-    })
-    })
-    .catch(err =>{
-    pass = 0
-    })
-    expect(pass).toEqual(1);
+    let x = 2 * 3;
+    expect(x).toEqual(6);
   });
