0cce3ef30e38b9a4b5729991ce37f6ccc1bdf2b6
Standing In The Hallway <110160969+StandingInTheHallway@users.noreply.github.com>
2022-10-07 03:21:10 +1300

    removed searchbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,6 @@ import CreateArticle from './components/CreateArticle';
 import ShowArticleList from './components/ShowArticleList';
 import ShowArticleDetails from './components/ShowArticleDetails';
 import UpdateArticleInfo from './components/UpdateArticleInfo';
-import SearchBar from "./components/SearchBar";
-import BookData from "./Data.json";
 
 
 import axios from 'axios';
@@ -17,12 +15,12 @@ class App extends Component {
   render() {
     return (
 
-        <><SearchBar placeholder="Enter a Book Name..." data={BookData} /><Routes>
+      <Routes>
         <Route exact path='/' element={<ShowArticleList />} />
         <Route path='/create-article' element={<CreateArticle />} />
         <Route path='/edit-article/:id' element={<UpdateArticleInfo />} />
         <Route path='/show-article/:id' element={<ShowArticleDetails />} />
-      </Routes></>
+      </Routes>
     );
   }
 }
