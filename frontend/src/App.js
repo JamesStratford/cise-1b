5d39bbd919d5d22983faeb45f37a30a611c13c85
JamesStratford <james.e.stratford@gmail.com>
2022-10-19 12:49:33 +1300

    hotfix to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,8 @@ import ShowArticleDetails from './components/ShowArticleDetails';
 import UpdateArticleInfo from './components/UpdateArticleInfo';
 
 
-import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:8082/";
+//import axios from 'axios';
+//axios.defaults.baseURL = "http://localhost:8082/";
 
 class App extends Component {
   render() {
